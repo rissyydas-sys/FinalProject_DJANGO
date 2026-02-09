8899ff0b49be15bc8e41ad541c494845a63eb611
rissyydas-sys <rissyydas@gmail.com>
2026-02-10 01:05:40 +0530

    Third LMS project commit

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-testkey'
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rissy.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
